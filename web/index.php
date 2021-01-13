0313568f8b94f43daf77a706f4b1c61b32cb38e7
sarah <sarah@DESKTOP-OAUA1DU>
2021-01-13 11:01:33 -0700

    added hompage  minimum requirements

--- web/index.php
+++ web/index.php
@@ -1,5 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giraffes</title>
+    <link rel="stylesheet" href="css/hpStyles.css" media="screen">
+</head>
+<body>
+    <h1>All About Giraffes</h1>
+    <div class="line"></div>
+    <a  href="https://www.pexels.com/photo/close-up-photography-of-giraffe-1382156/"><img class="giraffe" src="images/giraffe.jpeg" alt="giraffe"></a>
+    <p>The giraffe (Giraffa) is an African artiodactyl mammal, the tallest living terrestrial animal and the largest ruminant. It is traditionally considered to be one species, Giraffa camelopardalis, with nine subspecies. However, the existence of up to eight extant giraffe species has been described, based upon research into the mitochondrial and nuclear DNA, as well as morphological measurements of Giraffa. Seven other species are extinct, prehistoric species known from fossils.
 
-  phpinfo();
+</p><p>The giraffe's chief distinguishing characteristics are its extremely long neck and legs, its horn-like ossicones, and its distinctive coat patterns. It is classified under the family Giraffidae, along with its closest extant relative, the okapi. Its scattered range extends from Chad in the north to South Africa in the south, and from Niger in the west to Somalia in the east. Giraffes usually inhabit savannahs and woodlands. Their food source is leaves, fruits and flowers of woody plants, primarily acacia species, which they browse at heights most other herbivores cannot reach.
 
-?>
+</p><p>Giraffes may be preyed on by lions, leopards, spotted hyenas and African wild dogs. Giraffes live in herds of related females and their offspring, or bachelor herds of unrelated adult males, but are gregarious and may gather in large aggregations. Males establish social hierarchies through "necking", which are combat bouts where the neck is used as a weapon. Dominant males gain mating access to females, which bear the sole responsibility for raising the young.</p>
+</p>
+<p>The giraffe has intrigued various cultures, both ancient and modern, for its peculiar appearance, and has often been featured in paintings, books, and cartoons. It is classified by the International Union for Conservation of Nature as vulnerable to extinction, and has been extirpated from many parts of its former range. Giraffes are still found in numerous national parks and game reserves but estimates as of 2016 indicate that there are approximately 97,500 members of Giraffa in the wild. More than 1,600 were kept in zoos in 2010. -Wikipedia</p>
+
+</body>
+<footer>
+    <div>Sarah Parsons | Arizona | <a href="assignments.html">View Assignments</a> | <?php echo date('m-d-Y') ?></div>
+</footer>
+</html>
