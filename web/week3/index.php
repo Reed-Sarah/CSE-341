543029b1e205f232cb06b4015e5d673e88497363
sarah <sarah@DESKTOP-OAUA1DU>
2021-01-28 21:29:04 -0700

    testing

--- web/week3/index.php
+++ web/week3/index.php
@@ -4,6 +4,30 @@ include_once "library/functions.php";
 include_once "library/connection.php";
 include_once "model/products-model.php";
 
+function connectDB(){
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo "enter error block";
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    }
 
 $action = filter_input(INPUT_POST, 'action');
  if ($action == NULL){
