aa2db182cdd64f33a37a825830c7a38a8fd24a51
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-05 20:29:41 -0700

    testing

--- web/week3/snippets/header.php
+++ web/week3/snippets/header.php
@@ -7,6 +7,6 @@
 <li><a href="index.php?">All Clothing</a><li>
 <li><a href="index.php?action=filter&type=dress">Dresses</a><li>
 <li><a href="index.php?action=filter&type=top">Tops</a><li>
-<li><a href="index.php?action=filter&tye=bottom">Bottoms</a><li>
+<li><a href="index.php?action=filter&type=bottom">Bottoms</a><li>
 </ul>
 </nav>
