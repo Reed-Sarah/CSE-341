6e23ca240ee9eb45da8215c9bffee72d3754e5a9
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-05 20:13:19 -0700

    testing

--- web/week3/model/products-model.php
+++ web/week3/model/products-model.php
@@ -14,7 +14,7 @@ function getProductsByType($type) {
     $db = connectDB();
     $sql = 'SELECT * FROM products WHERE "type" = :types'; 
         $stmt = $db->prepare($sql); 
-        $stmt->bindValue(':types', $type, PDO::PARAM_STRING); 
+        $stmt->bindValue(':types', $type, PDO::PARAM_STR); 
         $stmt->execute(); 
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC); 
         $stmt->closeCursor(); 
