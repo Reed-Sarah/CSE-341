57cd7f600ec225b24908e071f20debb2e8787c4d
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-11 11:08:20 -0700

    debugging

--- web/week3/accounts/index.php
+++ web/week3/accounts/index.php
@@ -157,7 +157,7 @@ if($regOutcome === 1){
       
         $userInfo = getUserInfo($user_id, $db);
         $_SESSION['userData'] = $userInfo;
-        include '/week3/accounts/index.php?action=userUpdate';
+        include '/week3/accounts/?action=userUpdate';
         //header('Location: /week3/accounts/');
         exit;
        } else {
