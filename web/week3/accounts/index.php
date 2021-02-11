9b579409f61eab0f9794bf93cf480f9268b8b24d
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-11 10:51:14 -0700

    debugging

--- web/week3/accounts/index.php
+++ web/week3/accounts/index.php
@@ -157,8 +157,8 @@ if($regOutcome === 1){
       
         $userInfo = getUserInfo($user_id, $db);
         $_SESSION['userData'] = $userInfo;
-        //include '../views/manage-account.php';
-        header('Location: /week3/accounts/');
+        include '/week3/accounts/';
+        //header('Location: /week3/accounts/');
         exit;
        } else {
         $message = "<p>Sorry $first_name, the update failed. Please try again.</p>";
