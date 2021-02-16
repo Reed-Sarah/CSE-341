228496b664958442b5bbfb898f28155483151b23
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-16 16:09:22 -0700

    testing

--- web/teamActivity07_practice/model.php
+++ web/teamActivity07_practice/model.php
@@ -3,7 +3,7 @@ function signUpUser($db, $username, $hashedPassword) {
     $sql = 'INSERT INTO users07 (username, password) VALUES (:username, :hashedPassword)';
 $stmt = $db->prepare($sql);
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+$stmt->bindValue(':hashedPassword', $hashedPassword, PDO::PARAM_STR);
 $stmt->execute();
 $rowsChanged = $stmt->rowCount();
 $stmt->closeCursor();
