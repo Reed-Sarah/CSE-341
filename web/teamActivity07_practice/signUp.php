cf303c5331e3e8e565f96f5118689b4736780a18
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-17 10:16:49 -0700

    testing

--- web/teamActivity07_practice/signUp.php
+++ web/teamActivity07_practice/signUp.php
@@ -3,7 +3,7 @@
 <label>Username:</label><br>
 <input name="username" type=text><br>
 <label>Password:</label>
-<input name="password" type="password">
+<input name="password" type="password" pattern="[a-zA-Z\d\!@#\$%&\*]{7,}">
 <button type="submit">Sign Up</button>
 <input type="hidden" name="action" value="signUp">
 </form>
