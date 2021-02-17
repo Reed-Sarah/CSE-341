d6bdb812be968b6d2b4f9c8b1dc51a2673522167
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-17 10:23:06 -0700

    testing

--- web/teamActivity07_practice/signUp.php
+++ web/teamActivity07_practice/signUp.php
@@ -1,9 +1,9 @@
 <h1>Sign Up</h1>
 <form method="post" action="index.php">
 <label>Username:</label><br>
-<input name="username" type=text><br>
-<label>Password:</label>
-<input name="password" type="password" pattern="[a-zA-Z\d\!@#\$%&\*]{7,}">
+<input required name="username" type=text><br>
+<label>Password:</label><br>
+<input required name="password" type="password" pattern="[a-zA-Z\d\!@#\$%&\*]{7,}"><br>
 <button type="submit">Sign Up</button>
 <input type="hidden" name="action" value="signUp">
 </form>
