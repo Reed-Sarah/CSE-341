d8e17121cad1967bd10d97a8bd5fa09176b72153
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-16 21:13:07 -0700

    testing

--- web/teamActivity07_practice/index.php
+++ web/teamActivity07_practice/index.php
@@ -20,7 +20,7 @@ $action = filter_input(INPUT_POST, 'action');
         $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
         $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
         $userData = getUser($db, $username);
-        $verify = password_verify($password, $userData['hashedPassword']);
+        $verify = password_verify($password, $userData['password']);
 
         if(!$verify) {
             $SESSION['message'] = '<p class="notice">Please check your password and try again.</p>';
