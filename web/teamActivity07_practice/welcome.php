968e4c395185f763b8a879a232b02d2d35f4e8a7
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-16 21:44:35 -0700

    testing

--- web/teamActivity07_practice/welcome.php
+++ web/teamActivity07_practice/welcome.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
-// if (!isset($_SESSION['username'])) {
-//     header('location: index.php');
-//     exit;
-// }
+if (!isset($_SESSION['username'])) {
+    header('location: index.php');
+    exit;
+}
 echo "<h1>Welcome $_SESSION[username]";
