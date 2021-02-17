552619c6172d515bc4f5e6b022e5e2d23ec48769
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-16 21:27:10 -0700

    testing

--- web/teamActivity07_practice/index.php
+++ web/teamActivity07_practice/index.php
@@ -17,22 +17,23 @@ $action = filter_input(INPUT_POST, 'action');
         include 'signIn.php';
         break;
     case 'signIn':
+        header ('location: blank.php');
         echo "signing in ...";
-        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-        $userData = getUser($db, $username);
-        $verify = password_verify($password, $userData['password']);
+        // $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        // $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+        // $userData = getUser($db, $username);
+        // $verify = password_verify($password, $userData['password']);
 
-        if(!$verify) {
+        // if(!$verify) {
             
-            $_SESSION['message'] = '<p class="notice">Please check your password and try again.</p>';
-            include 'signIn.php';
-            echo "not verified";
-            exit;
-          }
+        //     $_SESSION['message'] = '<p class="notice">Please check your password and try again.</p>';
+        //     include 'signIn.php';
+        //     echo "not verified";
+        //     exit;
+        //   }
 
-          $_SESSION['username'] = $userData['username'];
-          header('location: welcome.php');
+        //   $_SESSION['username'] = $userData['username'];
+        //   header('location: welcome.php');
 
         break;
     default:
