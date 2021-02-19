88f7f011945033bb48d066408676829eb4599f0d
sarah <sarah@DESKTOP-OAUA1DU>
2021-02-19 16:20:47 -0700

    testing

--- web/week3/views/update-products.php
+++ web/week3/views/update-products.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Sarah's Boutique</title>
-        <link rel="stylesheet" href="../css/styles.css" media="screen">
+        <link rel="stylesheet" href="css/styles.css" media="screen">
     </head>
 <body>
     <div class="content">
